5a19fde78b7d8751a13ce55b19375ddc7a5f1377
den330 <wo131485508@qq.com>
2025-03-08 23:23:51 -0500

    setup

--- HP Trivia/HP_TriviaApp.swift
+++ HP Trivia/HP_TriviaApp.swift
@@ -1,32 +1,17 @@
 //
-//  HP_TriviaApp.swift
-//  HP Trivia
+//  tttApp.swift
+//  ttt
 //
 //  Created by yaxin on 2025-03-08.
 //
 
 import SwiftUI
-import SwiftData
 
 @main
-struct HP_TriviaApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
+struct tttApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(sharedModelContainer)
     }
 }
