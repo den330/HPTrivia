5d22be71436aac2dad4a33c6f9f82b9ddda64294
den330 <wo131485508@qq.com>
2025-05-16 22:03:05 -0400

    L3

--- HP Trivia/ContentView.swift
+++ HP Trivia/ContentView.swift
@@ -1,6 +1,11 @@
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    @State private var audioPlayer: AVAudioPlayer!
+    @State private var moveBackgroundImage = false
+    @State private var scalePlayButton = false
+    
     var body: some View {
         GeometryReader { geo in
             ZStack {
@@ -8,6 +13,12 @@ struct ContentView: View {
                     .resizable()
                     .frame(width: geo.size.width * 3, height: geo.size.height)
                     .padding(.top, 3)
+                    .offset(x: moveBackgroundImage ? geo.size.width / 1.1 : -geo.size.width / 1.1, y: 0)
+                    .onAppear {
+                        withAnimation(.linear(duration: 60).repeatForever()) {
+                            moveBackgroundImage.toggle()
+                        }
+                    }
                 VStack {
                     VStack {
                         Image(systemName: "bolt.fill")
@@ -57,6 +68,12 @@ struct ContentView: View {
                                 .clipShape(.rect(cornerRadius: 7))
                                 .shadow(radius: 5)
                         }
+                        .scaleEffect(scalePlayButton ? 1.2 : 1)
+                        .onAppear {
+                            withAnimation(.easeInOut(duration: 1.3).repeatForever()){
+                                scalePlayButton.toggle()
+                            }
+                        }
                         Spacer()
                         Button {
                             //
@@ -75,6 +92,15 @@ struct ContentView: View {
             .frame(width: geo.size.width, height: geo.size.height)
         }
         .ignoresSafeArea()
+        .onAppear {
+            playAudio()
+        }
+    }
+    private func playAudio() {
+        let sound = Bundle.main.path(forResource: "magic-in-the-air", ofType: "mp3")
+        audioPlayer = try! AVAudioPlayer(contentsOf: URL(filePath: sound!))
+        audioPlayer.numberOfLoops = -1
+        audioPlayer.play()
     }
 }
 
